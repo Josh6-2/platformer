c265cc371dae8d9810038ed612f95e98183d07fb
Joshua <pygamejg@gmail.com>
2023-08-18 19:31:37 +0100

    chamged jump to arrow up

--- main.py
+++ main.py
@@ -304,7 +304,7 @@ def main(window):
                 break
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player.jump_count < 2:
+                if event.key == pygame.K_UP and player.jump_count < 2:
                     player.jump()
 
         player.loop(FPS)
