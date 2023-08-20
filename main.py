c1ca67e744fe7b46192966a040174b5d09feb7fb
Joshua <pygamejg@gmail.com>
2023-08-20 16:10:13 +0100

    added extra terrain

--- main.py
+++ main.py
@@ -289,7 +289,14 @@ def main(window):
     floor = [Block(i * block_size, HEIGHT - block_size, block_size)
              for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
     objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size),
-               Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire]
+               Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire, 
+               Block(block_size * 4, HEIGHT - block_size * 5, block_size), 
+               Block(block_size * 7, HEIGHT - block_size * 5, block_size), 
+               Block(block_size * 10, HEIGHT - block_size * 4, block_size), 
+               Block(block_size * 13, HEIGHT - block_size * 3, block_size), 
+               Block(block_size * 14, HEIGHT - block_size * 3, block_size), 
+               Block(block_size * 15, HEIGHT - block_size * 3, block_size), 
+               Block(block_size * 16, HEIGHT - block_size * 3, block_size)]
 
     offset_x = 0
     scroll_area_width = 200
